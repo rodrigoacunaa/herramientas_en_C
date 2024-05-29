b211345ea85edd249ba506f0ddf734d4b5bbe1f9
Rodrigo Acuña <rodrigosebastianok@gmail.com>
2024-05-29 20:02:24 -0300

    Initial commit

---
+++ lib.h
@@ -0,0 +1,8 @@
+#ifndef LIB_H_INCLUDED
+#define LIB_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#endif // LIB_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib.h"
+//FILE*fopen(char*nombre,char*modo)
+// el nombre del archivo tiene que estar en la carpeta del proyecto por norma general
+int main()
+{
+
+}
